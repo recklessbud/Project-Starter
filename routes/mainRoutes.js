3fb05f3cdea3d2d2a3659153587fabcbf9e213ad
Alexander <alexander478@gmail.com>
2024-09-15 21:00:55 +0000

    new features to be added

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,8 +1,33 @@
 const express = require("express")
-
 const router = express.Router()
+const  {ensureAuth, ensureGuest} = require("../middleware/ensureAuth")
+const userAuth = require("../controller/userAuth")
 
 
+
+router.get("/", (req, res)=>{
+    res.render("login")
+})
+
+router.get("/dashboard", ensureAuth, async(req, res)=>{
+  try {
+    const user = req.user
+    res.render('dashboard', {user: user})
+  } catch (error) {
+    console.log(error)
+    res.send("page in progress")
+  }
+})
+
+                                    
+
+router.get("/signup", userAuth.getSignup)
+
+router.post("/login", userAuth.postLogin)
+
+router.post("/signup", userAuth.postSignup)
+
+router.get("/logout", userAuth.logout)
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,10 +8,51 @@ const session = require("express-session")
 const passport = require("passport")
 const cors = require("cors")
 
+const connectDB = require("./config/database")
+const mainRoutes = require("./routes/mainRoutes")
+const googleRoutes = require("./routes/googleAuth")
+
+require("dotenv").config({path: "./config/.env"})
+
+require("./config/passport")
 
 const apps = express()
+ 
+const PORT = process.env.PORT || 7070
+
+connectDB()
+ 
+apps.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({mongoUrl: process.env.MONGO_URI})
+    })
+)
+ 
 
 
 apps.use(cors())
 apps.use(express.json())
 apps.use(express.urlencoded({extended: "true"}))
+apps.set("view engine", "ejs")
+apps.use(express.static("public"))
+
+
+
+if(process.env.NODE_ENV === "development"){
+    apps.use(morgan("dev"))
+}
+
+apps.use(flash())
+apps.use(passport.initialize())
+apps.use(passport.session())
+
+
+apps.use("/", mainRoutes)
+apps.use("/auth", googleRoutes)
+
+apps.listen(PORT, ()=>{
+    console.log(`server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`)
+})
